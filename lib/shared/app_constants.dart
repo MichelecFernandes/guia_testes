6d5576bc32a81bbf1500e24d966b8f89eb29e5eb
MichelecFernandes <michelecristinafr@gmail.com>
2025-09-15 21:59:30 -0300

    feat: criacao das variaveis globais que serao utilizadas durante o desenvolvimento do guia

--- lib/shared/app_constants.dart
+++ lib/shared/app_constants.dart
@@ -1,4 +1,6 @@
 class AppConstants {
 
-  static const String appName = 'Login Exercicio';
+  static const String appName = 'Guia de Testes';
+  static const String appSlogan = 'Um guia completo para testes de software';
+  static const String appLoginMsg = 'Entre com seu acesso para continuar';
 }
