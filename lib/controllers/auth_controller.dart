979fb83ca546a3ee69393426ee64230ae85f3d2b
MichelecFernandes <michelecristinafr@gmail.com>
2025-10-05 12:28:52 -0300

    feat:  listagem dos cards de testes apos validacao do ogin

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:login_exercicio/models/post.dart';
 import 'package:login_exercicio/shared/app_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -64,29 +63,5 @@ class AuthController {
     await _sharedPreferences.clear();
     return true;
   }
-  Future<List<Post>> fetchGuideContent() async {
-    _sharedPreferences = await SharedPreferences.getInstance();
-    final token = _sharedPreferences.getString('accessToken');
 
-    if (token == null) {
-      throw Exception('Usuário não autenticado. Token ausente.');
-    }
-
-    final url = Uri.parse('${AppConstants.baseAuthApiUrl}posts');
-    final response = await http.get(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token', 
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final jsonBody = json.decode(response.body);
-      final postListResponse = PostListResponse.fromJson(jsonBody);
-      return postListResponse.posts;
-    } else {
-      throw Exception('Falha ao carregar os tópicos do guia. Código: ${response.statusCode}');
-    }
-  }
 }
