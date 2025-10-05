9038b48dca905b94bdc17df162aa3b6890d15980
MichelecFernandes <michelecristinafr@gmail.com>
2025-10-05 12:23:44 -0300

    feat: adicao de links de curso na estrutura da guia de testes

--- lib/models/guide_topic.dart
+++ lib/models/guide_topic.dart
@@ -1,11 +1,20 @@
+class CourseLink {
+  final String title;
+  final String url;
+
+  CourseLink({required this.title, required this.url});
+}
+
 class GuideTopic {
   final String title;
   final String link;
-  final String description; 
+  final String description;
+  final List<CourseLink> courses; 
 
   GuideTopic({
     required this.title,
     required this.link,
-    this.description = ''
+    this.description = '',
+    required this.courses,
   });
 }
