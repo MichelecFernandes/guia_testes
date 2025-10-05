2975f5f3746030179a479f0a99490bf2f4f01b9e
MichelecFernandes <michelecristinafr@gmail.com>
2025-10-05 12:16:50 -0300

    feat: adicao da api publica utilizada para autenticacao

--- lib/shared/app_constants.dart
+++ lib/shared/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
-
   static const String appName = 'Guia de Testes';
   static const String appSlogan = 'Um guia completo para testes de software';
   static const String appLoginMsg = 'Entre com seu acesso para continuar';
+  static const String baseAuthApiUrl = 'https://dummyjson.com/';
 }
