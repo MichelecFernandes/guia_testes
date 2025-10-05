373bd1228dc981edf11bfac69d809aa5d7d941f4
MichelecFernandes <michelecristinafr@gmail.com>
2025-10-05 11:49:31 -0300

    feat: criacao da estrutura de tipos de teste para listagem

---
+++ lib/models/guide_topic.dart
@@ -0,0 +1,11 @@
+class GuideTopic {
+  final String title;
+  final String link;
+  final String description; 
+
+  GuideTopic({
+    required this.title,
+    required this.link,
+    this.description = ''
+  });
+}
